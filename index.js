c481aed3ff9802c932d9538cfd75730d7ac6db65
Hailey Sampson <haileysampson840@gmail.com>
2025-02-21 18:18:11 -0400

    Updated index.js with L5 Exercise

--- index.js
+++ index.js
@@ -1,5 +1,119 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+//object literal
+let bob = {
+  fname: "bob",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1603',
+      grades: [ 89, 34, 67 ]
+    },
+    {
+      course: 'INFO 1601',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
 
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
+let sally = {
+  fname: "sally",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1601',
+      grades: [ 100, 89, 79 ]
+    }
+  ]
+};
+
+let paul = {
+  fname: "paul",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1600',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
+
+
+const students = [bob, sally, paul];
+
+// Exercise
+function getAverageGrade(student, course){
+  let sum = 0;
+  let average = 0;
+  let count = 0;
+
+  for(let i = 0; i < students.length; i++){
+    if(students[i].fname === student){
+      for(let c = 0; c < students[i].transcript.length; c++){
+        if(students[i].transcript[c].course === course){
+          for(let j = 0; j < students[i].transcript[c].grades.length; j++){
+            sum = sum + students[i].transcript[c].grades[j];
+            count++;
+          }
+        } else{
+          return -1;
+        }
+      }
+    }
+  }
+
+  average = sum / count;
+  return average;
 }
+
+// Test
+console.log(getAverageGrade('sally', 'INFO 1603'));
+
+function getAssignmentMark(student, course, num){
+  let grade;
+
+  for(let i = 0; i < students.length; i++){
+    if(students[i].fname === student){
+      for(let c = 0; c < students[i].transcript.length; c++){
+        if(students[i].transcript[c].course === course){
+          grade = students[i].transcript[c].grades[num];
+          return grade;
+        } else{
+          return -1;
+        }
+      }
+    } 
+  }
+}
+
+// Test
+console.log(getAssignmentMark('sally', 'INFO 1601', 1));
+
+function averageAssessment(students, courseName, assignment){
+  let sum = 0;
+  let average = 0;
+  let count = 0;
+
+  for(let i = 0; i < students.length; i++){
+    for(let c = 0; c < students[i].transcript.length; c++){
+      if(students[i].transcript[c].course === courseName){
+        for(let j = 0; j < students[i].transcript[c].grades.length; j++){
+          if(j === assignment){
+            sum = sum + students[i].transcript[c].grades[j];
+            count++;
+          }          
+        }
+      }
+    }
+  }
+
+  average = sum/count;
+  return average;
+}
+
+// Test
+console.log(averageAssessment(students, 'INFO 1601', 1));
